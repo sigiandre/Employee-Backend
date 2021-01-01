8d8f93a9fbc7cc3dd0bda151a637433ec2822d5b
Andre Diaz <andrediaz@MacBook-Pro-de-Andre.local>
2021-01-01 04:18:56 -0500

    Employee Backend

---
+++ EmployeeRegisterAPI/Models/EmployeeDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeRegisterAPI.Models
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options){}
+
+        public DbSet<EmployeeModel> Employees { get; set; }
+    }
+}
---
+++ EmployeeRegisterAPI/Models/EmployeeModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+
+namespace EmployeeRegisterAPI.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+
+        [Column(TypeName="nvarchar(50)")]
+        public string EmployeeName { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Occupation { get; set;}
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string ImageName { get; set; }
+
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
+
+        [NotMapped]
+        public string ImageSrc { get; set; }
+    }
+}
